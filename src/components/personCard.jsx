88992e7df94d0d78a9f62c716d0613633e6480b4
Ruben Amarilla <daniel.amarilla.rda@gmail.com>
2023-01-21 16:11:43 -0300

    version final

--- src/components/personCard.jsx
+++ src/components/personCard.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+
 class PersonCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            personAge: this.props.age,
+        };
+    }
+
+    plusOneAge = () => {
+        let plusOne = this.state.personAge+1;
+        this.setState({personAge: plusOne});
+    }
+
     render () {
-        let myStyle = {
-            margin: "5px auto",
-        }
-        let { name, lastName, age, hairColor } = this.props;
+        let { name, lastName, hairColor } = this.props;
         return (
-            <div className="container" style={myStyle}>
+            <div className="container">
                 <h1>{ lastName }, { name }</h1>
-                <p>Age: { age }</p>
+                <p>Age: { this.state.personAge }</p>
                 <p>Hair Color: { hairColor }</p>
+                <button onClick={ this.plusOneAge }>Birthday Button for { name } { lastName }</button>
             </div>
         );
     }
